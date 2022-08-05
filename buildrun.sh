3a9adf31e63e877a86ad0dead073aef61c87e7ab
root <johanlj@axis.com>
2022-08-05 14:08:36 +0000

    Improve test script

--- buildrun.sh
+++ buildrun.sh
@@ -3,6 +3,15 @@
 cam=192.168.0.13
 port=1234
 
+# model=ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite
+# width=300
+# height=300
+
+model=mobilenet_v2_1.0_224_quant_edgetpu.tflite
+width=300
+height=300
+
+
 # Build and install ACAPs
 
 . /opt/axis/acapsdk/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
@@ -12,9 +21,9 @@ acap-build . -m manifest-armv7hf.json
 
 # eap-install.sh $cam pass install
 
-CXXFLAGS="$CXXFLAGS -g0 -DTEST" acap-build . -m manifest-test.json -a 'testdata/*'
-rm ACAP_Runtime_1_1_0_armv7hf.eap
-eap-install.sh $cam pass install
+# CXXFLAGS="$CXXFLAGS -g0 -DTEST" acap-build . -m manifest-test.json -a 'testdata/*'
+# rm ACAP_Runtime_1_1_0_armv7hf.eap
+# eap-install.sh $cam pass install
 
 # Copy binaries to device
 
@@ -31,7 +40,8 @@ sleep 2
 
 cp apis/tensorflow_serving/apis/prediction_service.proto ./apis
 
-apis/grpcurl --import-path /opt/app_host/apis --proto videocapture.proto --plaintext -d '{"settings": { "format": "3", "width": 300, "height": 300, "framerate": 10, "timestamp_type": "1" }}' \
+# # Format 2 is jpg, format 3 is yuv
+apis/grpcurl --import-path /opt/app_host/apis --proto videocapture.proto --plaintext -d '{"settings": { "format": "3", "width": '$width', "height": '$height', "framerate": 10, "timestamp_type": "1" }}' \
  $cam:$port videocapture.VideoCapture/NewStream | jq --raw-output .streamId > temp
 
 stream=$(cat temp)
@@ -39,16 +49,20 @@ rm temp
 
 # read -n 1 -p "Press key"
 
-apis/grpcurl --import-path /opt/app_host/apis --proto prediction_service.proto --plaintext -d '{ "inputs": { "data": { "dtype": 22, "uint32_val": '$stream', "tensor_shape": { "dim": [{"size": 1}, {"size": 300}, {"size": 300}, {"size": 2}] } }  }, "model_spec": { "name": "/var/spool/storage/SD_DISK/models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite"  }  }' $cam:$port tensorflow.serving.PredictionService/Predict
+apis/grpcurl --import-path /opt/app_host/apis --proto videocapture.proto --plaintext -d '{ "stream_id": '$stream'}' $cam:$port videocapture.VideoCapture/GetFrame \
+ | jq --raw-output .data | base64 --decode > img
 
-apis/grpcurl --import-path /opt/app_host/apis --proto videocapture.proto --plaintext -d '{ "stream_id": '$stream'}' $cam:$port videocapture.VideoCapture/GetFrame | tail -c 100
+apis/grpcurl --import-path /opt/app_host/apis --proto prediction_service.proto --plaintext -d '{ "inputs": { "data": { "dtype": 22, "uint32_val": '$stream', "tensor_shape": { "dim": [{"size": 1}, {"size": 300}, {"size": 300}, {"size": 2}] } }  }, "model_spec": { "name": "/var/spool/storage/SD_DISK/models/'$model'"  }  }' $cam:$port tensorflow.serving.PredictionService/Predict
+
+# | tail -c 100
 
 rm ./apis/prediction_service.proto
 
-sshpass -p pass ssh root@$192.168.0.13 "killall acapruntime"
+sshpass -p pass ssh root@$cam "killall acapruntime"
 
 # Run test binary
 
-sshpass -p pass ssh root@$cam "/usr/local/packages/acapruntimetest/acapruntimetest --gtest_color=yes" &
-sleep 2
+# sshpass -p pass ssh root@$cam "/usr/local/packages/acapruntimetest/acapruntimetest --gtest_filter='-Inference*'" & #--gtest_color=yes
+
+# sshpass -p pass ssh root@$192.168.0.13 "killall acapruntimetest"
 
